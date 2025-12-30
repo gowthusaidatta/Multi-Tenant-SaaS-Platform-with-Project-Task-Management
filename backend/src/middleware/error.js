e567411b834eaf1d96c894480a9190cb96db0d40
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2025-12-30 15:31:43 +0530

    sloveing errors

--- backend/src/middleware/error.js
+++
@@ -1,5 +0,0 @@
-export function errorHandler(err, req, res, next) {
-  // eslint-disable-next-line no-console
-  console.error(err);
-  res.status(500).json({ success: false, message: 'Internal server error' });
-}
